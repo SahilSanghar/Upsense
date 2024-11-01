46f7a9ff712cca3f51702d6e8105c92f2d6a894d
SAHIL sanghar <sangharsahil546@gmail.com>
2024-11-01 17:37:59 +0530

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',         // Optimize for Vercel deployment
+    reactStrictMode: true,         // Enable React strict mode for highlighting potential issues
+    swcMinify: true, 
+};
 
 export default nextConfig;
